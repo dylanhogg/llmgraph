d43981f01bbda11b8ed526ea675fd36021926474
Dylan Hogg <dylanhogg@gmail.com>
2024-06-30 19:29:20 +1000

    Bump package versions to latest July (#9)
    * Bump package versions to latest.
    * Remove need for pkg_resources
    * Bump to v1.2.3

--- llmgraph/library/consts.py
+++ llmgraph/library/consts.py
@@ -1,7 +1,5 @@
-import pkg_resources
-
 package_name = "llmgraph"
-version = pkg_resources.get_distribution(package_name).version
+version = "1.2.3"
 prompts_yaml_location = "prompts.yaml"
 
 default_llm_model = "gpt-3.5-turbo"
